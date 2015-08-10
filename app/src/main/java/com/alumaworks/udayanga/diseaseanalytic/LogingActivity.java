d26322a9223222d0fb624c89a031d20eee9ca2f3
Udayanga Senanayake <u.senanayake91@gmail.com>
2015-08-10 15:42:17 +0530

    Version 1.0

--- app/src/main/java/com/alumaworks/udayanga/diseaseanalytic/LogingActivity.java
+++ app/src/main/java/com/alumaworks/udayanga/diseaseanalytic/LogingActivity.java
@@ -1,8 +1,14 @@
 package com.alumaworks.udayanga.diseaseanalytic;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.parse.GetCallback;
@@ -24,24 +31,58 @@ public class LogingActivity extends ActionBarActivity {
     EditText editTextUserName,editTextPassword;
     Button login_button,sign_up_button;
     SharedPreferences sharedPreferences;
+    private boolean connection=false;
+    private ProgressBar progressBar=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loging);
+        sharedPreferences = getSharedPreferences("UserData",MODE_PRIVATE);
 
-        sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
-        editTextUserName = (EditText) findViewById(R.id.editTextUserName);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        //make if user logged previosly,log automaticaly
-        boolean hasloggedinPreviously = sharedPreferences.getBoolean("hasloggedinPreviously", false);
-        String user_name = sharedPreferences.getString("user_name", "dummyUsername");
-        if (hasloggedinPreviously) {
-            Intent intent = new Intent(LogingActivity.this, MapsActivity.class);
-            startActivity(intent);
-            finish();
+        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        progressBar =(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
 
+        connection=isInternetConnected(getApplication());
+        if(connection){
+
+            //make if user logged previosly,log automaticaly
+
+            boolean hasloggedinPreviously=sharedPreferences.getBoolean("hasloggedinPreviously",false);
+            String username=sharedPreferences.getString("username","dummyUsername");
+            if(hasloggedinPreviously){
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }
+        else {
+            AlertDialog.Builder builder=new AlertDialog.Builder(LogingActivity.this);
+            builder.setMessage("Please enable data connection")
+                    .setCancelable(false)
+                    .setTitle("Data Connection Status")
+                    .setPositiveButton("Turn ON",
+                            new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialog,int id){
+                                    Intent myIntent=new Intent(
+                                            Settings.ACTION_WIRELESS_SETTINGS);
+                                    startActivity(myIntent);
+                                    dialog.cancel();
+
+                                }
+                            })
+                    .setNegativeButton("Exit",
+                            new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialog,int id){
+                                    finish();
+                                }
+                            });
+            AlertDialog alert=builder.create();
+            alert.show();
+        }
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,50 +106,99 @@ public class LogingActivity extends ActionBarActivity {
     }
 
     public void onClickLoginButton(View view) {
-        Log.d("LOGIN", "Login button was called");
+        progressBar.setVisibility(View.VISIBLE);
+        connection=isInternetConnected(getApplication());
+        if(connection){
+            Log.d("LOGIN", "Login button was called");
 
-        Parse.initialize(this, "eHVWp9eSv1VnZqMnIKGAvTnnADc8EeaGdVe0SH41", "m4UFyOkG7fM9kqE9r1Gt1ffLe7IpbLgY3V3Fcbgt");
+            Parse.initialize(this, "eHVWp9eSv1VnZqMnIKGAvTnnADc8EeaGdVe0SH41", "m4UFyOkG7fM9kqE9r1Gt1ffLe7IpbLgY3V3Fcbgt");
 
-        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
-        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+            editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+            editTextPassword=(EditText)findViewById(R.id.editTextPassword);
 
-        final String user_name = editTextUserName.getText().toString();
-        final String password = editTextPassword.getText().toString();
+            final String user_name = editTextUserName.getText().toString();
+            final String password = editTextPassword.getText().toString();
 
-        Log.d("LOGIN", "Username : " + user_name + ", Password : " + password);
+            Log.d("LOGIN", "Username : " + user_name + ", Password : " + password);
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("user");
+            query.whereEqualTo("user_name", user_name);
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("user");
-        query.whereEqualTo("user_name", user_name);
-        query.getFirstInBackground(new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
-                if (object == null) {
-                    Toast.makeText(getApplicationContext(), "Object is null", Toast.LENGTH_SHORT).show();
-                } else {
-                    Log.d("score", "Retrieved the object.");
-                    if(object.getString("password").equals(password)){
-                        //save login data
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("hasLoggedinPreviously", true);
-                        editor.putString("user_name", user_name);
-                        editor.commit();
-                        //start new activity
-                        Intent intent = new Intent(LogingActivity.this, MapsActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(getApplicationContext(), "Wrong username & password", Toast.LENGTH_SHORT).show();
+            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                public void done(ParseObject object, ParseException e) {
+                    if (object == null) {
+                        Toast.makeText(getApplicationContext(), "Object is null", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String testUserName=object.getString("user_name");
+                        String testPassword=object.getString("password");
+                        if(testUserName.equals(user_name)&&testPassword.equals(password)) {
+                            Intent intent = new Intent(LogingActivity.this, MapsActivity.class);//call the second acyivity
+                            //create xml to save data
+                            SharedPreferences.Editor editor=sharedPreferences.edit();
+                            editor.putBoolean("hasloggedinPreviously",true);
+                            editor.putString("username",user_name);
+                            editor.commit();
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Wrong username & password", Toast.LENGTH_SHORT).show();
 
+                        }
                     }
                 }
-            }
-        });
+            });
 
 
 
+        }
+        else {
+            progressBar.setVisibility(View.INVISIBLE);
+            AlertDialog.Builder builder=new AlertDialog.Builder(LogingActivity.this);
+            builder.setMessage("Please enable data connection")
+                    .setCancelable(false)
+                    .setTitle("Data Connection Status")
+                    .setPositiveButton("Turn ON",
+                            new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialog,int id){
+                                    Intent myIntent=new Intent(
+                                            Settings.ACTION_WIRELESS_SETTINGS);
+                                    startActivity(myIntent);
+                                    dialog.cancel();
 
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialog,int id){
+                                    finish();
+                                }
+                            });
+            AlertDialog alert=builder.create();
+            alert.show();
+        }
     }
     public void onClickSignUpButton(View view) {
         Intent intent = new Intent(LogingActivity.this, SignupActivity.class);
         startActivity(intent);
 
     }
+
+    public static boolean isInternetConnected (Context ctx) {
+        ConnectivityManager connectivityMgr = (ConnectivityManager) ctx
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        // Check if wifi or mobile network is available or not. If any of them is
+        // available or connected then it will return true, otherwise false;
+        if (wifi != null) {
+            if (wifi.isConnected()) {
+                return true;
+            }
+        }
+        if (mobile != null) {
+            if (mobile.isConnected()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/alumaworks/udayanga/diseaseanalytic/MapsActivity.java
+++ app/src/main/java/com/alumaworks/udayanga/diseaseanalytic/MapsActivity.java
@@ -1,13 +1,23 @@
 package com.alumaworks.udayanga.diseaseanalytic;
 
 import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -18,18 +28,31 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity  {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private RatingBar ratingBar;
     private Button buttonGetLocation,buttonSendLocation;
     private EditText editLattitude,editTextLongitude;
+
+    //get location variables
+    private LocationManager locationManager=null;
+    private LocationListener locationListener=null;
+    private ProgressBar progressBar=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
-   }
+        //set display always portrait
+       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //start progressbar will not work
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    }
 
     @Override
     protected void onResume() {
@@ -79,21 +102,47 @@ public class MapsActivity extends FragmentActivity {
         ratingBar=(RatingBar)findViewById(R.id.ratingBar);
         editLattitude=(EditText)findViewById(R.id.editLattitude);
         editTextLongitude=(EditText)findViewById(R.id.editTextLongitude);
-
         buttonGetLocation=(Button)findViewById(R.id.buttonGetLocation);
         buttonGetLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MapsActivity.this, String.valueOf(
-                        "Rating :"+ratingBar.getRating())+
-                        "\nLatitude :"+editLattitude.getText().toString()+
-                        "\nLongitude :"+editTextLongitude.getText().toString()
-                        , Toast.LENGTH_SHORT).show();
+                  //check GPS enable
+                ContentResolver contentResolver=getBaseContext().getContentResolver();
+                boolean gpgStatus=Settings.Secure.isLocationProviderEnabled(contentResolver,LocationManager.GPS_PROVIDER);
 
+                if(gpgStatus){//if GPS enable
+                    progressBar.setVisibility(View.VISIBLE);//start progress bar working
+                    locationListener=new getMyLocation();//call to getMyLocation class
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,10,locationListener);
+                    }
+                else {//GPS enable Aleart
+                    AlertDialog.Builder builder=new AlertDialog.Builder(MapsActivity.this);
+                    builder.setMessage("Please enable GPS")
+                            .setCancelable(false)
+                            .setTitle("GPS Status")
+                            .setPositiveButton("Turn ON",
+                                    new DialogInterface.OnClickListener(){
+                                        public void onClick(DialogInterface dialog,int id){
+                                            Intent myIntent=new Intent(
+                                                    Settings.ACTION_SEARCH_SETTINGS);
+                                            startActivity(myIntent);
+                                            dialog.cancel();
+
+                                        }
+                                    })
+                            .setNegativeButton("Cancel",
+                                    new DialogInterface.OnClickListener(){
+                                        public void onClick(DialogInterface dialog,int id){
+                                            dialog.cancel();
+                                        }
+                                    });
+                    AlertDialog alert=builder.create();
+                    alert.show();
+                }
             }
         });
     }
-    public void onSendButtonClick(View view){
+    public void onSendButtonClick(View view){//send data to online database
         Parse.initialize(this, "eHVWp9eSv1VnZqMnIKGAvTnnADc8EeaGdVe0SH41", "m4UFyOkG7fM9kqE9r1Gt1ffLe7IpbLgY3V3Fcbgt");
         AlertDialog.Builder builder=new AlertDialog.Builder(MapsActivity.this);
         builder.setMessage("Send data to the system?"+"Rating :"+ratingBar.getRating()+
@@ -129,4 +178,38 @@ public class MapsActivity extends FragmentActivity {
         alertDialog.show();
 
     }
+private class getMyLocation implements LocationListener{
+    private Location location;
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location=location;
+        progressBar.setVisibility(View.INVISIBLE);
+        //set edit text's text to lattitude and longitude
+        editLattitude.setText(String.valueOf(location.getLatitude()));
+        editTextLongitude.setText((String.valueOf(location.getLatitude())));
+
+        Toast.makeText(MapsActivity.this, String.valueOf(
+                "Rating :"+ratingBar.getRating())+
+                "\nLatitude :"+String.valueOf(location.getLatitude())+
+                "\nLongitude :"+String.valueOf(location.getLongitude())
+                , Toast.LENGTH_SHORT).show();
+   }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
+
+
 }
